6946c67aac5d3b666590f966df511169ab076ccd
shuxuezhang <441879728@qq.com>
2018-09-22 18:25:57 +0800

    aa

--- test.js
+++ test.js
@@ -44,14 +44,23 @@ for(let i = 0 ;i<maps.keys().length; i++){
 /**
  * git 的4种状态  untracked未被追踪的  Modified工作区修改 还没有保存到暂存区
  *               staged：添加到暂存区还没有到版本库
+ *
  * git status 状态查询
+ * git log --oneline
  * git add   放到暂存区   git add .  一次提交所有的内容
  * git commit -m 提交暂存区内容到版本库
  * git commit -am 合并上面两个操作
- * git commit --amend 打补丁（）
+ * git commit --amend 打补丁（撤销上一次的提交，并将暂存区的内容重新提交， 也可以用来修改描述）
+ *                    可以先git add 然后 git commit --amend
  * git checkout <文件名> 拉取暂存区的内容 替换工作区的内容
  * git reset Head 拉取最近一次版本库中的文件替换到暂存区  工作区不变
  * git diff 比较工作区 与 暂存区的差别
+ *
+ *
+ * git删除
+ * 方法1 在本地删除  然后 git add . 提交删除操作  git commit
+ * 方法2 git rm --cached <filename> 只删除暂存区文件 
+ *      git rm -f  强行将修改的文件也删除
  */
 
 //如果想要让工作区的内容  恢复到上个版本  可以先git reset head   然后 git checkout
