aa373aaed9ed0279683fa8284acbe2b7d32348f5
shuxuezhang <441879728@qq.com>
2018-09-23 09:56:44 +0800

    test2

--- test.js
+++ test.js
@@ -1,46 +1,4 @@
-
-let  array = [['hello', 'bill'],['hello','jack'], ['world','foo','jerry'],['world','foo','pretter','tom']];
-
-let json = [];
-let maps = new Map();
-let father_arr = [];
-for (let i = 0 ; i<4; i++ ){
-	
-	let data = new Map();
-	for(let j = 0; j<array.length; j++){
-		let father;
-		let name;
-		let arr = array[j];
-		// console.log(arr)
-		if(i < arr.length){
-			name = arr[i];
-			console.log(name)
-			if(i == 0){
-				father = null
-			}else{
-				father = arr[i-1];
-			}
-		}
-		if(name){
-			data.set(name,father);
-		}
-		
-	}
-
-	maps.set(i, data);	
-}
-
-console.log(maps.keys().length);
-let tree = [];
-for(let i = 0 ;i<maps.keys().length; i++){
-	if(i == 0){
-		let data = '';
-	}
-}
-
-// 第一版本
-
-
+//test2
 /**
  * git 的4种状态  untracked未被追踪的  Modified工作区修改 还没有保存到暂存区
  *               staged：添加到暂存区还没有到版本库
